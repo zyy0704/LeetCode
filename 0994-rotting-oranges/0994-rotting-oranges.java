7ecb6f104d8cef4e57c7c38d9156d417b05e9ac3
linna <96746924+zyy0704@users.noreply.github.com>
2022-10-29 17:53:28 -0700

    Time: 2 ms (97.17%), Space: 42.8 MB (61.43%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,57 @@
+class Solution {
+    List<int[]> rotten = new ArrayList<>();
+    int fresh = 0;
+    public int orangesRotting(int[][] grid) {
+        
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 2){
+                    floodFill(grid, i, j, 2);
+                }
+            }
+        }
+    
+        
+    int mins = 2;
+        
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    return -1;
+                } else{
+                    mins = Math.max(mins,grid[i][j]);
+                }
+            }
+        }
+        //System.out.println(rotten.get(0)[0]);
+        return mins - 2;
+    }
+    
+    private void floodFill(int[][] grid, int x, int y, int mins){
+        
+        if(x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] == 0 || (1 < grid[x][y] && grid[x][y] < mins)){
+            return;
+        }else {
+            grid[x][y] = mins;
+            fresh--;
+            
+            if(x < grid.length ){
+            floodFill(grid, x + 1, y , mins + 1);
+        }
+            
+            if(y < grid[0].length){
+                floodFill(grid, x, y + 1, mins + 1);
+            }
+            
+            if(x > 0){
+                floodFill(grid, x - 1, y, mins + 1);
+            }
+            
+            if(y > 0){
+             floodFill(grid, x, y - 1, mins + 1);   
+            }  
+            
+        }
+        
+    }
+}
